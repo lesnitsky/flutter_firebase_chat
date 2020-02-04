ce50cf51f65977d53912753019ef3fb02ea54618
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2020-02-04 14:07:16 +0300

    add appbar

--- lib/screens/chat_list_screen.dart
+++ lib/screens/chat_list_screen.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class ChatListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('Chats')),
+      body: Container(),
+    );
   }
 }
