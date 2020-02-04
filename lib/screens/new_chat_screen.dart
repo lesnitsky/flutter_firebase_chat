7b3ead00e419513ac70086d78db0b56f828cbc22
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2020-02-04 14:18:06 +0300

    add label

--- lib/screens/new_chat_screen.dart
+++ lib/screens/new_chat_screen.dart
@@ -14,6 +14,7 @@ class NewChatScreen extends StatelessWidget {
             Expanded(
               child: TextField(
                 autofocus: true,
+                decoration: InputDecoration(labelText: 'Chat title'),
               ),
             ),
             FlatButton(
