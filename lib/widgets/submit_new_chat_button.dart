2e9ed310e9a9bfd02d4982bedb904fb3c7cb562c
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2020-02-04 14:29:54 +0300

    handle text input

--- lib/widgets/submit_new_chat_button.dart
+++ lib/widgets/submit_new_chat_button.dart
@@ -13,8 +13,27 @@ class _SubmitNewChatButtonState extends State<SubmitNewChatButton> {
   bool disabled = false;
 
   @override
+  void initState() {
+    widget.controller.addListener(_onTextChanged);
+    super.initState();
+  }
+
+  _onTextChanged() {
+    if (widget.controller.text.length > 0 && disabled) {
+      setState(() {
+        disabled = false;
+      });
+    } else if (widget.controller.text.length == 0) {
+      setState(() {
+        disabled = true;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return FlatButton(
+      textColor: Colors.blue,
       onPressed: disabled ? null : () {},
       child: Text('Create'),
     );
