139191e885efe9c9d8110760f756175a5afb0960
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2020-02-04 14:05:34 +0300

    add chat list screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ void main() {
 class FirebaseChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //
+    return MaterialApp(
+      home: ChatListScreen(),
+    );
   }
 }
